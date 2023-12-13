8cb52fdfc1f1d23afa55f1d86a618d1c537b9a71
farandeffraim <93232915+farandeffraim@users.noreply.github.com>
2023-12-13 15:01:47 +0700

    Add files via upload

---
+++ projek1.py
@@ -0,0 +1,8 @@
+panjang = int(input("Masukkan panjang : "))
+lebar = int(input("Masukkan lebar : "))
+
+luas = panjang * lebar
+keliling = 2*(panjang + lebar)
+
+print("Luas persegi panjang : ",luas)
+print("Keliling persegi panjang : ",keliling)
